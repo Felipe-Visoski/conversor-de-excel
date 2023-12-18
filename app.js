1e665342cd36ba3cdae0b4fcd45a223189137961
Felipe Visoski Dos Santos <felipepb35@gmail.com>
2023-12-18 19:00:54 -0300

    18-12

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const { table } = require("console");
 var Reader = require("./Reader");
-var Processor = require("./processor")
+var Processor = require("./processor");
+var Table = require("./table");
+var HtmlParser = require("./HtmlParser");
+
 
 
 
@@ -10,5 +14,9 @@ var leitor = new Reader();
 async function main(){
     var dados = await leitor.Read("./usuarios.csv");
     var dadadosprocessados = Processor.Process(dados);
+
+    var usuarios = new Table(dadadosprocessados);
+   var html = await HtmlParser.Parse(usuarios);
+   console.log(html);
 }
 main();
---
+++ table.js
@@ -0,0 +1,19 @@
+class table{
+    constructor(arr){
+        this.header = arr[0];
+        arr.shift();
+        this.rows = arr;
+
+    };
+
+    get RowsCount(){
+        return this.rows.length;
+    };
+
+    get ColumnCount(){
+        return this.header.length;
+    }
+}
+
+
+module.exports = table;
